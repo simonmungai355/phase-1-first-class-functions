901ba53e4483069d366b75aad469965bbb73a7b0
simonmungai355 <simon.mungai1@student.moringaschool.com>
2023-08-02 10:38:03 +0300

    assignment complete

---
+++ index.js
@@ -0,0 +1,22 @@
+function receivesAFunction( callback){
+
+    callback()
+
+}
+function returnsANamedFunction() {
+}
+
+function returnsANamedFunction() {
+    
+    return function namedFunction() {
+    
+    };
+  }
+
+  function returnsAnAnonymousFunction() {
+   
+    return function() {
+e
+    };
+  }
+  
